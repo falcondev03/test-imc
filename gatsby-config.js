26b68e1b49ba1fa5c414de38a8ff97088a4858a1
Rene Garcia <rsamuelgarcia@gmail.com>
2020-05-27 12:58:54 -0400

    agregados los nombres y la metadata de la app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Oh-Style.me`,
+        description: `Test Imc, cursos y apoyo para ayudarte a adelgazar y estar en forma`,
+        author: `Dylia`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `OhStyle.me`,
+                short_name: `ohstyle`,
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
